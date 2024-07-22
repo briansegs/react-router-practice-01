62fdc38136d715bfc8e039f44b96a0d9eaf43030
bsegers <briansegs@gmail.com>
2024-07-22 09:38:51 -0400

    Create basic app structure

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import ReactDom from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout, Home, Blogs, Contact, NoPage } from "./pages";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
