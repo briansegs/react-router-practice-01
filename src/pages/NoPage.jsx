e37c8f55aacbbb41cd7bce1545b0a20d60dec15e
bsegers <briansegs@gmail.com>
2024-07-22 10:34:08 -0400

    Add back button

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NoPage = () => {
-  return <h1>404</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ paddingBottom: "30px" }}>
+      <h1>404</h1>
+      <h3>Page not found</h3>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+    </div>
+  );
 };
 
 export default NoPage;
